74bc633f9d440572a6979a62e965cb775b6d376b
MadinaKon <141622998+MadinaKon@users.noreply.github.com>
2025-07-10 14:46:06 +0200

    created demo accounts

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,13 @@
+import path from "path";
+
 export default {
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   server: {
+    port: 3000,
     allowedHosts: [
       "localhost",
       "127.0.0.1",
